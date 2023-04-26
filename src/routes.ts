0316f3fac573e9f4c2d3b79bfea8a598f633afa1
Sousa <joou@gft.com>
2023-04-26 14:55:31 -0300

    Configurando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { CreateCreditCardController } from "./controllers/creditcard/CreateCreditCardController";
+import { AuthCreditCardController } from "./controllers/creditcard/AuthCreditCardController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailsCreditCardController } from "./controllers/creditcard/DetailsCreditCardController";
 
 const router = Router();
 
 // ------- Rotas para CreditCard ------- //
 router.post("/creditcard", new CreateCreditCardController().handle);
+router.post("/login", new AuthCreditCardController().handle);
+router.get(
+  "/cardinfo",
+  isAuthenticated,
+  new DetailsCreditCardController().handle
+);
 
 export { router };
