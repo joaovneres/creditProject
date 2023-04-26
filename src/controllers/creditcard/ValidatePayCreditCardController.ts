8473a1c71818037f5f755cbf6deb0fff6b482c83
Sousa <joou@gft.com>
2023-04-26 16:03:58 -0300

    Refatorando código

--- src/controllers/creditcard/ValidatePayCreditCardController.ts
+++ src/controllers/creditcard/ValidatePayCreditCardController.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from "express";
-import { ValidatePayCreditCardService } from "../../services/creditcard/ValidatePayCreditCardService";
 
 class ValidatePayCreditCardController {
   async handle(req: Request, res: Response) {
-    const authToken = req.headers.authorization;
-    const validatePayCreditCardService = new ValidatePayCreditCardService();
-    const validatePay = await validatePayCreditCardService.execute(authToken);
-    return res.json(validatePay);
+    return res.json({ status: "Compra autorizada" });
   }
 }
 
